889ca86002cac11eb62958dcbe3c102d54630d72
Deniz Keskin <avalonalgo@protonmail.com>
2024-03-02 07:36:34 +0200

    Added pdf files

---
+++ components/Footer.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <footer class="rounded-lg shadow bg-gray-900">
+      <div
+        class="w-full mx-auto p-4 md:flex md:items-center md:justify-between"
+      >
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+          >© 2024 E-HVAC
+        </span>
+        <ul
+          class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
+        >
+          <li>
+            <NuxtLink to="" class="hover:underline me-4 md:me-6"
+              >About</NuxtLink
+            >
+          </li>
+          <li>
+            <NuxtLink
+              external
+              to="/GİZLİLİK-POLİTİKASI.pdf"
+              target="_blank"
+              class="hover:underline me-4 md:me-6"
+              >Gizlilik Politikasi</NuxtLink
+            >
+          </li>
+          <li>
+            <NuxtLink
+              external
+              to="/KULLANICI-SÖZLEŞMESİ-(Üyelik Sözleşmesi).pdf"
+              target="_blank"
+              class="hover:underline me-4 md:me-6"
+              >Kullanici Sozlesmesi</NuxtLink
+            >
+          </li>
+          <li>
+            <NuxtLink
+              external
+              to="/KVKK-AYDINLATMA METNİ.pdf"
+              target="_blank"
+              class="hover:underline me-4 md:me-6"
+              >KVKK Aydinlatma Metni</NuxtLink
+            >
+          </li>
+
+          <li>
+            <NuxtLink to="" class="hover:underline">Contact</NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  </div>
+</template>
